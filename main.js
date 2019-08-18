6e1314ee5be37b96d784ec0ae2a88c2fcf96a00e
motomotoyoshi <motomoto.git@gmail.com>
2019-08-19 02:41:13 +0900

    update, main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 {
-    document.body.textContent = 'Hello';
-    document.title = 'Rick Dom';
+    // document.body.textContent = 'Hello';
+    // document.title = 'Rick Dom';
+    document.getElementById('target').textContent = 'Change';
 }
