2dfff8d070a0fe88736c8de2774ff93ce8b41d93
motomotoyoshi <motomoto.git@gmail.com>
2019-08-21 00:59:57 +0900

    update, main.js

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 'use strict';
 
 {
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Title';
-    document.body.appendChild(h1);
+    const text = document.querySelector('input[type="text"]');
+    const textarea = document.querySelector('textarea');
 
-    const p = document.createElement('p');
-    p.textContent = 'Hello, hell.';
-    document.body.appendChild(p);
+    console.log(text.value);
+    console.log(textarea.value);
 
-    const h2 = document.createElement('h2');
-    h2.textContent = 'sub title';
-    document.body.insertBefore(h2, p);
+    // text.focus();
+    text.select();
 
-    const copy = p.cloneNode(true);
-    document.body.insertBefore(copy, h2);
-
-    document.body.removeChild(h2);
+    textarea.disabled = true;
 }
