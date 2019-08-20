6085fba3567406bd50b566762ff878805055e093
motomotoyoshi <motomoto.git@gmail.com>
2019-08-21 01:12:36 +0900

    update, main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 'use strict';
 
 {
-    const text = document.querySelector('input[type="text"]');
-    const textarea = document.querySelector('textarea');
+    // console.log(document.querySelectorAll('input[type="checkbox"]')[0].checked);
+    // console.log(document.querySelectorAll('input[type="checkbox"]')[1].checked);
 
-    console.log(text.value);
-    console.log(textarea.value);
-
-    // text.focus();
-    text.select();
-
-    textarea.disabled = true;
+    document.querySelectorAll('input[type="checkbox"]')[0].checked = true;
+    console.log(document.querySelectorAll('input[type="radio"]')[0].checked);
+    console.log(document.querySelectorAll('input[type="radio"]')[1].checked);
 }
+    console.log(document.querySelectorAll('select > option')[0].selected);
+    console.log(document.querySelectorAll('select > option')[1].selected);
+    console.log(document.querySelectorAll('select > option')[2].selected);
